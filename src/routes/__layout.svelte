83955bc0597bf76f5bf48aa9f3b578be05cd9350
Steven Bruno <sbruno636@gmail.com>
2021-11-07 19:16:46 -0500

    create mobile landing page

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,26 @@
+<nav class="flex justify-end px-3 py-2 md:hidden">
+	<div class="text-gray-500">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-8 w-8"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="3"
+				d="M4 6h16M4 12h16M4 18h16"
+			/>
+		</svg>
+	</div>
+</nav>
+
+<slot />
+
+<style>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Steven Bruno</title>
+</svelte:head>
+
+<div class="px-4 mb-20">
+	<p class="text-6xl font-bold text-gray-800 uppercase my-5 tracking-wide">Steven Bruno</p>
+	<p class="text-xl font-bold text-gray-600 uppercase tracking-widest mb-1">Product Designer</p>
+	<p class="text-xl font-bold text-gray-600 uppercase tracking-widest mb-1">Based in NYC</p>
+	<p class="text-xl font-bold text-gray-600 uppercase tracking-widest mb-1">
+		UX @ <a href="https://pos.toasttab.com" class="text-blue-500 hover:text-blue-600">Toast</a>
+	</p>
+</div>
+<div class="flex flex-col items-end px-4 gap-6">
+	<a
+		href="projects"
+		class="text-blue-500 uppercase font-bold tracking-widest text-xl border-b-4 border-gray-300 hover:text-blue-600"
+		>View Projects</a
+	>
+	<a
+		href="writing"
+		class="text-blue-500 uppercase font-bold tracking-widest text-xl border-b-4 border-gray-300 hover:text-blue-600"
+		>View Writing</a
+	>
+</div>
