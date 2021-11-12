2bd853fbdd7cf1d9f745e3143b651ab1efcc6ad0
Steven Bruno <sbruno636@gmail.com>
2021-11-11 22:56:09 -0500

    revert from graphic style to normal style

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,22 +3,34 @@
 </svelte:head>
 
 <div class="px-4 mb-20">
-	<p class="text-6xl font-bold text-gray-800 uppercase my-5 tracking-wide">Steven Bruno</p>
-	<p class="text-xl font-bold text-gray-600 uppercase tracking-widest mb-1">Product Designer</p>
-	<p class="text-xl font-bold text-gray-600 uppercase tracking-widest mb-1">Based in NYC</p>
-	<p class="text-xl font-bold text-gray-600 uppercase tracking-widest mb-1">
-		UX @ <a href="https://pos.toasttab.com" class="text-blue-500 hover:text-blue-600">Toast</a>
+	<p class="text-2xl font-semibold font-serif text-gray-800 my-4 dark:text-gray-300">
+		Steven Bruno
 	</p>
+	<div class="flex flex-col ">
+		<p class="mb-2 text-gray-500 leading-7 dark:text-gray-400">
+			Hi, I'm Steven. I'm a product designer based in NYC. Currently, I'm building web and mobile
+			applications at Toast.
+		</p>
+		<p class="mb-2 text-gray-500 leading-7 dark:text-gray-400">
+			I'm a UX generalist with diverse experience across the design ecosystem. To learn more about
+			my background, check out my <a
+				class="text-blue-500 dark:text-blue-400 hover:text-blue-600 font-medium"
+				href="/">resume</a
+			>.
+		</p>
+		<p class="mb-2 text-gray-500 leading-7 dark:text-gray-400">
+			Send me an email if you'd like to say hello. <a
+				class="text-blue-500 dark:text-blue-400 hover:text-blue-600 font-medium"
+				href="/">sbruno636@gmail.com</a
+			>
+		</p>
+	</div>
 </div>
-<div class="flex flex-col items-end px-4 gap-6">
-	<a
-		href="projects"
-		class="text-blue-500 uppercase font-bold tracking-widest text-xl border-b-4 border-gray-300 hover:text-blue-600"
-		>View Projects</a
+<div class="flex flex-col px-4 gap-4">
+	<a href="projects" class="text-blue-500 dark:text-blue-400 font-medium hover:text-blue-600"
+		>View projects</a
 	>
-	<a
-		href="writing"
-		class="text-blue-500 uppercase font-bold tracking-widest text-xl border-b-4 border-gray-300 hover:text-blue-600"
-		>View Writing</a
+	<a href="writing" class="text-blue-500 dark:text-blue-400 font-medium hover:text-blue-600"
+		>View blog posts</a
 	>
 </div>
