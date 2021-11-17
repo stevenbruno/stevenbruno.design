7d492a43ea24668ab8eb3df6de34ab97ded637e4
Steven Bruno <sbruno636@gmail.com>
2021-11-17 14:27:24 -0500

    udpate link styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,15 +3,15 @@
 </svelte:head>
 
 <div class="px-4 mb-12">
-	<p class="font-medium text-gray-800 my-4 dark:text-gray-300">Steven Bruno</p>
+	<p class="font-medium text-gray-800 my-4 dark:text-gray-300">Hi, I'm Steven.</p>
 	<div class="flex flex-col ">
 		<p class="mb-2 text-gray-500 leading-7 dark:text-gray-400">
 			I'm a product designer based in NYC. Currently, I'm building restaurant technology at Toast.
 		</p>
 		<p class="mb-2 text-gray-500 leading-7 dark:text-gray-400">
-			Previously, I studed Civil Engineering at Northwestern University. I planned on becoming an
-			architect, but those plans changed once I took some computer science classes and learned how
-			much I enjoyed designing software.
+			Previously, I studed Civil Engineering at Northwestern University, where I planned on becoming
+			an architect. Those plans changed once I took some computer science classes and learned how
+			much I enjoy designing software.
 		</p>
 		<p class="mb-2 text-gray-500 leading-7 dark:text-gray-400">
 			These days, I’m thinking and reading about climate solutions, blockchain possibilities, and
@@ -19,17 +19,17 @@
 		</p>
 		<p class="mb-2 text-gray-500 leading-7 dark:text-gray-400">
 			To get in touch, reach me at <a
-				class="text-blue-600 dark:text-blue-400 hover:text-blue-600 font-medium"
+				class="text-blue-600 dark:text-blue-500 underline hover:text-blue-600"
 				href="/">sbruno636@gmail.com</a
 			>
 		</p>
 	</div>
 </div>
 <div class="flex flex-col px-4 gap-4">
-	<a href="projects" class="text-blue-500 dark:text-blue-400 font-medium hover:text-blue-600"
+	<a href="projects" class="text-blue-600 dark:text-blue-500 underline hover:text-blue-600"
 		>View projects</a
 	>
-	<a href="writing" class="text-blue-500 dark:text-blue-400 font-medium hover:text-blue-600"
+	<a href="writing" class="text-blue-600 dark:text-blue-500 underline hover:text-blue-600"
 		>View blog posts</a
 	>
 </div>
