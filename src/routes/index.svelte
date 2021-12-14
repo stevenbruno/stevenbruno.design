fe1f9620065816af674ee6fe32f3b46f84da8bbe
Steven Bruno <sbruno636@gmail.com>
2021-12-13 21:22:06 -0500

    add social links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,18 +12,20 @@
 <!-- For some reason I need to reference a dark mode class here in order for the dark mode on the html to work -->
 <div class="max-w-screen-lg mx-auto pt-24 text-gray-500 dark:text-slate-400 dark:bg-darkbg">
 	<div class="mb-24">
-		<h1 class="text-xl font-medium tracking-wide text-blurple mb-1">Steven Bruno</h1>
-		<p class="max-w-2xl mb-3 leading-relaxed">
+		<h1 class="text-xl font-medium tracking-wide text-blurple mb-1">Hi, I'm Steven</h1>
+		<p class="max-w-2xl mb-2 leading-relaxed">
 			I'm a product designer based in New York City. Currently, I'm designing software for
 			restaurants at Toast. To get in touch, reach me at
 			<span class="underline hover:text-blurple"
 				><a href="mailto:sbruno636@gmail.com">sbruno636@gmail.com</a></span
 			>.
 		</p>
-		<div class="flex gap-3 text-lg text-gray-400">
-			<i class="fab fa-linkedin-in text-xl align-bottom leading-6" />
-			<i class="fab fa-github align-bottom leading-6" />
-			<i class="fab fa-twitter align-bottom leading-6" />
+		<div class="flex gap-3 items-end text-lg text-gray-400">
+			<a href="https://www.linkedin.com/in/stevenjbruno/"
+				><i class="fab fa-linkedin-in text-xl hover:text-blurple" /></a
+			>
+			<a href="https://github.com/stevenbruno"><i class="fab fa-github hover:text-blurple" /></a>
+			<a href="https://twitter.com/sbruno626"><i class="fab fa-twitter hover:text-blurple" /></a>
 		</div>
 	</div>
 	<div class="flex flex-col gap-16 lg:flex-row lg:gap-0 justify-between mb-8">
