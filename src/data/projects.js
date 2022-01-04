05c11612b0dbf97fb9296928c8fd47011a7b294f
Steven Bruno <sbruno636@gmail.com>
2022-01-04 15:58:39 -0500

    update side projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,6 +3,17 @@ const projects = [
 		name: 'stevenbruno.design',
 		description: 'My personal website built with Svelte and Tailwind CSS',
 		url: '/#'
+	},
+	{
+		name: 'Grid Sketch',
+		description: 'A vanilla Javascript desktop drawing game that utilizes CSS Grid',
+		url: '/#'
+	},
+	{
+		name: 'Exoplanet Detection with One-Class Classification',
+		description:
+			'Exploring machine learning techniques for detecting habitable exoplanets. Part of Chicago Python User Group mentorship program.',
+		url: 'https://dev.to/stevenbruno/an-introduction-to-one-class-classification-2m5c'
 	}
 ];
 
